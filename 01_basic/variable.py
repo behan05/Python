a66287e00dee566c5509687d71101706250c64f1
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-01-21 03:58:09 +0000

    variable

---
+++ 01_basic/variable.py
@@ -0,0 +1,18 @@
+# Variable a like container that contain different type of data..
+
+name = "tony"
+age = 24
+height = 5.6
+fav_color = ["red", "blue", "green", "orange", "yellow"]
+
+# print(name, age, height, fav_color)
+
+print(name)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(fav_color))
+
+
+
