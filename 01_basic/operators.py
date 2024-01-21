d96d5d279523e33aac8bf03057a85915a9a5850f
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-01-21 04:19:39 +0000

    operators

---
+++ 01_basic/operators.py
@@ -0,0 +1,70 @@
+# this is a kind of symbol.
+
+# Arithmetic operator
+
+"""num = 5
+num2 = 10
+
+print(num + num2)
+print(num - num2)
+print(num * num2)
+print(num / num2)
+print(num ** num2)
+print(num % num2)"""
+
+# unary
+# ++ --
+
+# Assignment operator
+
+"""num1 = 5
+num2 = 10
+total = 0
+
+total += num1 + num2
+total -= num1 + num2
+total /= num1 + num2
+total *= num1 + num2
+total **= num1 + num2
+total %= num1 + num2"""
+
+# print(total)
+
+# comparison operator
+
+"""num = 5
+num2 = 10
+
+print(num == num2)
+print(num != num2)
+print(num >= num2)
+print(num <= num2)
+print(num > num2)
+print(num < num2)"""
+
+# Logical operator
+
+"""num = 5
+num2 = 10
+
+print(num == num2 and num > num2)
+print(num != num2 or num > num2)
+print(not(num != num2))
+"""
+
+# identity operator
+
+"""num = 5
+num2 = 10
+
+print(num is num2)
+print(num is not num2)"""
+
+# membership operator
+
+"""sentence = "brown fox jumps over the lazy dogs"
+print("dogs" in sentence)
+print("dogs" not in sentence)
+"""
+# bitwish operator
+
