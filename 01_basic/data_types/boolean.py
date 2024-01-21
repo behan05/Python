903fa9344f99878d9d90d689a9503b065ac19426
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-01-21 04:55:53 +0000

    boolean daata type

---
+++ 01_basic/data_types/boolean.py
@@ -0,0 +1,25 @@
+
+# boolean value => True & Flase
+
+
+num = 5
+num2 = 10
+
+# all are return boolean data type.
+
+print(num == num2)
+print(num != num2)
+print(num > num2)
+print(num >= num2)
+print(num < num2)
+print(num <= num2)
+
+print(num is num2)
+print(num is not  num2)
+
+print(num >= num2 and num == num2)
+print(num >= num2 or num < num2)
+print(not (num >= num2))
+
+print('num' in 'num2')
+print('num' not in 'num2')
