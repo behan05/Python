cac90d5195933d347b700d60f7bbe9a5c7433402
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-01-21 04:37:45 +0000

    integer data type

---
+++ 01_basic/data_types/number/integer.py
@@ -0,0 +1,27 @@
+
+# int()
+# integer always return whole value..
+
+num = 25
+print(f"type of {num} is {type(num)}")
+
+print(num.bit_length())
+
+num2 = float(num)
+print(f"type of {num2} is {type(num2)}")
+
+
+num3 = int(num2)
+print(f"type of {num3} is {type(num3)}")
+
+
+
+
+
+
+
+
+
+
+
+
