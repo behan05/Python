bb14347f5e040bec8476f1f04f385a046c5e0017
Behan kumar <83676505+behan05@users.noreply.github.com>
2024-01-21 04:44:41 +0000

    learn more about complex number and float number

---
+++ 01_basic/data_types/number/complex.py
@@ -0,0 +1,12 @@
+# complex number can mostly used in data science.
+
+num = 123j
+
+print(num + 223j)
+print(num - 223j)
+print(num * 223j)
+print(num ** 223j)
+print(num / 223j)
+
+print(type(num))
+
---
+++ 01_basic/data_types/number/float.py
@@ -0,0 +1,10 @@
+
+num = 25.5
+print(f"type of {num} is {type(num)}")
+
+num2 = int(num)
+print(f"type of {num2} is {type(num2)}")
+
+
+num3 = float(num2)
+print(f"type of {num3} is {type(num3)}")
