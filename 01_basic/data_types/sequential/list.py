348d97d38853b248f1a24a1d60d1f8eab2a0662e
behan05 <behankrbth@gmail.com>
2024-01-28 23:56:15 +0530

    learn more about list data type

---
+++ 01_basic/data_types/sequential/list.py
@@ -0,0 +1,45 @@
+# mutable .
+
+num = [1,2,3,4,5,6]
+# print(num.__dir__())
+# print(type(num))
+# print(num)
+# print(num[0]) # access items by index, start from 0.
+# res = []
+# for i in num:
+#     res.append(i)
+# res.reverse()
+# print(res)   
+# print(num.count(2))
+# res = num.copy()
+# num.pop()
+
+# num.index(2)
+# num.clear()
+# num.insert(0,3)
+# num.remove(3)
+# num2 = [3,21,6,4,3,9]
+# num2.sort()
+# print(num2)
+
+# num = []
+# for i in range(5+1):
+#     random_Input = input("inter a num: ")
+#     num.append(random_Input)
+# num.sort()
+# print(num)
+
+"""num = []
+for i in range(2):
+    random_Input = int(input("inter a num: "))
+    num.append(random_Input)
+
+if (len(num) != 0):
+    for i in num:
+        if (i > 18):
+            print("you can vote!")
+            break
+        else:
+            print("you can not vote! because your age is: {}".format(i))
+            break
+"""
